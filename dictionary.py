62c253ae49e36b0e51551877868f698fe5d3c0b6
Zablon Oigo <imjunior254@gmail.com>
2023-03-02 21:59:48 +0300

    python dictionary

--- dictionary.py
+++ dictionary.py
@@ -31,3 +31,7 @@ for x in people:
     print(x)   
     #print key and value 
     print(people[x])
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  
+square_dict = {n: n*n for n in list1}  
+print(square_dict)  

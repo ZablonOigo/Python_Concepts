32107f6244209884bb01600c392b07a991eaaaa3
Zablon Oigo <imjunior254@gmail.com>
2023-03-03 22:25:58 +0300

    manipulating data using json

--- datastring.py
+++ datastring.py
@@ -3,3 +3,15 @@ import json
 x='{"name":"john", "age":30, "city":"New York"}'
 detail=json.loads(x)
 print(detail['age'])
+
+
+# conver python to json
+data={
+    'name':'Doe',
+    'age':'40',
+    'city':'London'
+
+}
+# convert into json
+y=json.dumps(data)
+print(y)

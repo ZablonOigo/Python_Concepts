95cc5939dc03c2b5de6cf2e60ca3ad64b042db6d
Zablon Oigo <imjunior254@gmail.com>
2023-03-04 21:32:15 +0300

    reverse a string

--- basic_module.py
+++ basic_module.py
@@ -6,4 +6,4 @@ def divide(x,y):
     return x/y 
 def multiply(x,y):
     return x*y
-  if __name__ == '__main__':               
+if __name__ == '__main__' :
---
+++ reverse.py
@@ -0,0 +1,7 @@
+txt='software developer'[::-1]
+print(txt)
+
+
+def myreverse(x):
+    return x[::-1]
+print(myreverse('hello'))

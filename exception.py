e4926775f13777b63c7407e02f8cf4e6c25307b0
Zablon Oigo <imjunior254@gmail.com>
2023-03-04 21:58:29 +0300

    python exception

---
+++ exception.py
@@ -0,0 +1,18 @@
+'''
+The try block lets you test a block of code for errors.
+
+The except block lets you handle the error.
+
+The else block lets you execute code when there is no error.
+
+The finally block lets you execute code, regardless of the result of the try- and except blocks.
+'''
+
+try:
+    print('hello')
+except:
+    print('something went wrong') 
+
+else:
+    print('nothing went wrong')
+           
